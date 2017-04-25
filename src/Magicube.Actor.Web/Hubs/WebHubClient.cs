cb84af3c94d484a2408411c3223a74fb589b23e1
changwei.zhao <changwei@we7.cn>
2017-04-25 18:24:58 +0800

    web init

---
+++ src/Magicube.Actor.Web/Hubs/WebHubClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Magicube.Actor.Web.Hubs {
+    [HubName("WebHubClient")]
+    public class WebHubClient : Hub {
+        private const string WebIdentity = "BROWSERS";
+
+
+
+        public override System.Threading.Tasks.Task OnConnected() {
+            if (Context.Headers.Get("ORLEANS") != "GRAIN") {
+                Groups.Add(Context.ConnectionId, WebIdentity);
+            }
+            return base.OnConnected();
+        }
+    }
+}
